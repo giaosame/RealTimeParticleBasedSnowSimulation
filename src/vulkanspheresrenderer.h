80f712c4e7ab65d4f20069b3591fd54d7022969f
giaosame <qzcqs@icloud.com>
2020-12-14 03:42:46 -0500

    update window name

--- src/vulkanspheresrenderer.h
+++ src/vulkanspheresrenderer.h
@@ -175,7 +175,7 @@ private:
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Snow Simulator", nullptr, nullptr);
         glfwSetWindowUserPointer(window, this);
         glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
 
